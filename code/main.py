59bd8a47bca9bfd9f21197f2634249c654ef42e1
ThomasTBO <Thomas.THEBAULT@student.isae-supaero.fr>
2025-08-21 14:18:32 +0200

    main

--- code/main.py
+++ code/main.py
@@ -12,18 +12,22 @@ if __name__ == "__main__":
     3. Plot the results
     """
     
-    year = "2018"
-    month = "01"
+    year = "2017"
+    month = "11"
     
     path = "./Cryosat_RSR_SAR_FBR_" + year + "_" + month        # To change as you like
 
+    # Password for accessing the Cryosat FTP server : 
+    # Username : anonymous ; Password : your mail adress
+    password = None
+
     # Step 1 : Extract PSEP
     # /!\ Make sure you downloaded the txt file required before running (cf ReadMe)
-    extract_psep(path, year, month)
+    extract_psep(path, year, month, password=password)
     
     # Step 2 : Apply RSR
     # /!\ Make sure you dowloaded the rsr package AND applied the modifications by launching the script rsr_package_modification.py
     apply_rsr_arctic(path)
 
     # Step 3 : Plot the results
-    plot_rsr_results(path, year, month, latlon_target_list=None)  # You can provide a list of target lat/lon pairs to plot their distributions
+    plot_rsr_results(path, year, month, latlon_target_list=[(72.09971834404213,-62.87869659584134)])  # You can provide a list of target lat/lon pairs to plot their distributions
